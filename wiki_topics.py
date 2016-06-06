314b1854aed8b62ace716dec0ee73062b13f74d3
huangwaleking <huangwaleking@gmail.com>
2016-06-07 03:24:04 +0800

    generate wiki.topics

--- wiki_topics.py
+++ wiki_topics.py
@@ -1,5 +1,6 @@
 #generate wiki.topics from ~/git/wiki_processor/data/features/
 import os
+from operator import itemgetter
 
 def loadFeatures(filename):
     d=dict()
@@ -17,13 +18,16 @@ def loadFeatures(filename):
 
 
 featureFileFolder="../wiki_processor/data/features"
+#todo, replace it by ../wiki_processor/additional_scripts/topicnames.txt
 files=os.listdir(featureFileFolder)
 wikiTopicsFile=open("wiki.topics","w")
 idx=0
 for f in files:
     print(f)
     d=loadFeatures(featureFileFolder+"/"+f)
-    for word in d:
-        wikiTopicsFile.write("topic%s\t%s\t%s\n" % (idx,word,d[word]))
+    sortedWords=sorted(d.items(),key=itemgetter(1),reverse=True)
+    for t in sortedWords:
+        word,score=t
+        wikiTopicsFile.write("topic%s\t%s\t%s\n" % (idx,word,score))
     idx=idx+1
 wikiTopicsFile.close()
