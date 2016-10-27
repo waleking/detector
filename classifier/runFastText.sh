c4142588ddca8073973c042311a6accdbaff4626
huangwaleking <huangwaleking@gmail.com>
2016-10-28 00:02:08 +0800

    ready to change to lncs format

--- classifier/runFastText.sh
+++ classifier/runFastText.sh
@@ -1,4 +1,4 @@
-./fasttext supervised -input "data/20newsgroup.train" -output "result_fastText/20newsgroup" -dim 10 -lr 0.1 -wordNgrams 2 -minCount 1 -bucket 10000000 -epoch 640 -thread 4
+./fasttext supervised -input "data/20newsgroup.train" -output "result_fastText/20newsgroup" -dim 10 -lr 0.1 -wordNgrams 2 -minCount 1 -bucket 10000000 -epoch 320 -thread 4
 ./fasttext test result_fastText/20newsgroup.bin data/20newsgroup.test
 ./fasttext predict result_fastText/20newsgroup.bin data/20newsgroup.test > result_fastText/20newsgroup.test.predict
 
